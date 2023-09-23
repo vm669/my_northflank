876189dc5a33fbb57c71b3e2d36b034656d6b139
vm669 <82027738+vm669@users.noreply.github.com>
2023-09-23 18:51:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,25 +14,30 @@ mv v ${RELEASE_RANDOMNESS}
 cat config.json | base64 > config
 rm -f config.json
 
-# NEZHA_SERVER="data.langyun.pp.ua"
-# NEZHA_PORT=443
-# NEZHA_KEY="O9tf61G2XHupyVUHDx"
+NEZHA_SERVER="data.langyun.pp.ua"
+NEZHA_PORT=443
+NEZHA_KEY="O9tf61G2XHupyVUHDx"
 
 # 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-TLS=${NEZHA_TLS:+'--tls'}
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS}
+# TLS=${NEZHA_TLS:+'--tls'}
+# [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS}
 
 
 generate_nezha() {
   cat > nezha.sh << EOF
 #!/usr/bin/env bash
 
+# 哪吒的三个参数
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
+
 # 检测是否已运行
 check_run() {
-  [[ \$(pgrep -lafx nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
+  [[ \$(pidof nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
 }
 
-# 若哪吒三个变量不全，则不安装哪吒客户端
+# 三个变量不全则不安装哪吒客户端
 check_variable() {
   [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
 }
@@ -40,17 +45,22 @@ check_variable() {
 # 下载最新版本 Nezha Agent
 download_agent() {
   if [ ! -e nezha-agent ]; then
-    URL=\$(wget -qO- "https://api.github.com/repos/nezhahq/agent/releases/latest" | grep -o "https.*linux_amd64.zip")
-    URL=\${URL:-https://github.com/nezhahq/agent/releases/download/v0.15.6/nezha-agent_linux_amd64.zip}
-    wget \${URL}
-    unzip -qod ./ nezha-agent_linux_amd64.zip
-    rm -f nezha-agent_linux_amd64.zip
+    URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
+    wget -t 2 -T 10 -N \${URL}
+    unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
   fi
+}
+
+# 运行客户端
+run() {
+  [ -e nezha-agent ] && chmod +x nezha-agent && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY} --tls
 }
 
 check_run
 check_variable
 download_agent
+run
+wait
 EOF
 }
 
